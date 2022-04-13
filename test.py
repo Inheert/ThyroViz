1844bade3c82e79583e59f181af3c87cbd8def46
Théo CLAEREBOUDT <90567698+Inheert@users.noreply.github.com>
2022-04-13 23:00:44 +0200

    liens utiles

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+# https://www.canva.com/colors/color-palette-generator/
+# https://colordesigner.io
+# https://colorpicker.fr/#download
+# https://getbootstrap.com/docs/4.1/utilities/shadows/
+# https://icons.getbootstrap.com/icons/eye-fill/
 from pages.helpers import *
 
 test = GetCategoryPercent()
